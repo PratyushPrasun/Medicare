818609cfaf9c37296867269e20d583b78eca0faa
Pratyush <138666886+PratyushPrasun@users.noreply.github.com>
2026-04-11 22:08:31 +0530

    Update CORS allowed origins
    Added new allowed origins for CORS configuration.

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,8 @@ const app = express();
 const port = 4000;
 
 const allowedorigins = [
-    "http://localhost:5173",
-    "http://localhost:5174",
+    "https://medicare-eight-nu.vercel.app",
+    "https://medicare-u5uv.vercel.app",
 ];
 
 app.use(cors(
