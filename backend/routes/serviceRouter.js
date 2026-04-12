3544831ce04a79a813aa4ff706878a0da4679673
Pratyush <138666886+PratyushPrasun@users.noreply.github.com>
2026-04-12 12:26:25 +0530

    Update serviceRouter.js

--- backend/routes/serviceRouter.js
+++ backend/routes/serviceRouter.js
@@ -1,15 +1,21 @@
 import express from "express";
-import multer from "multer";
-import { createService, deleteService, getServiceById, getServices, updateService } from "../controllers/serviceController.js";
+import upload from "../config/multer.js";
+import {
+  createService,
+  deleteService,
+  getServiceById,
+  getServices,
+  updateService
+} from "../controllers/serviceController.js";
 
-const upload = multer({dest:"/temp"});
 const serviceRouter = express.Router();
 
 serviceRouter.get("/", getServices);
 serviceRouter.get("/:id", getServiceById);
 
-serviceRouter.post('/', upload.single("image"), createService);
+serviceRouter.post("/", upload.single("image"), createService);
 serviceRouter.put("/:id", upload.single("image"), updateService);
 
 serviceRouter.delete("/:id", deleteService);
+
 export default serviceRouter;
